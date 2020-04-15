078a985dd4e44fb183f8feb6182771ee03a0e438
Gavin Stark <gjstark@me.com>
2020-04-15 14:08:14 +0100

    [cdl_desc] Builds with apb too

--- Makefile
+++ Makefile
@@ -1,19 +1,19 @@
 CDL_ROOT=/Users/gavinprivate/Git/cdl_tools_grip/tools
 include ${CDL_ROOT}/lib/cdl/cdl_templates.mk
 SRC_ROOT   = $(abspath ${CURDIR})
+OTHER_SRCS = ${SRC_ROOT}/../*
 BUILD_ROOT = ${SRC_ROOT}/build
-LIBRARY_NAME = atcf_hardware_std
-
-SIM ?= $(abspath ${CURDIR})/sim
 
 all: sim
+
+-include ${BUILD_ROOT}/Makefile
+
+$(eval $(call cdl_makefile_template,${SRC_ROOT},${BUILD_ROOT},${OTHER_SRCS}))
+
+clean:
+	rm -rf ${BUILD_ROOT}
+	mkdir -p ${BUILD_ROOT}
 
 smoke: sim
 	(cd test && PATH=${SRC_ROOT}/python:${PATH} ${MAKE} SIM=${SIM} smoke)
 
--include ${BUILD_ROOT}/Makefile
-
-$(eval $(call library_makefile_template,${LIBRARY_NAME},${SRC_ROOT},${BUILD_ROOT}))
-
-clean:
-	mkdir -p ${BUILD_ROOT}
--- library_desc.py
+++ library_desc.py
@@ -1,26 +1,29 @@
 import cdl_desc
 from cdl_desc import CdlModule, CModel
 
+class Library(cdl_desc.Library):
+    name="std"
+    pass
+
 class TechModules(cdl_desc.Modules):
     name = "tech"
     c_src_dir   = "cmodel"
     src_dir     = "cdl"
     tb_src_dir  = "tb_cdl"
     include_dir = "cdl"
+    export_dirs = [ src_dir, include_dir ]
     modules = []
     modules += [ CdlModule("tech_sync_bit") ]
     modules += [ CdlModule("tech_sync_flop") ]
-    modules += [ CModel("srams",src_dir=c_src_dir) ]
     pass
 
-class SramhModules(cdl_desc.Modules):
+class SramModules(cdl_desc.Modules):
     name = "sram"
     c_src_dir   = "cmodel"
     src_dir     = "cdl"
     tb_src_dir  = "tb_cdl"
     include_dir = "cdl"
+    export_dirs = [ src_dir, include_dir ]
     modules = []
     modules += [ CModel("srams",src_dir=c_src_dir) ]
     pass
-
-modules=cdl_desc.Modules.__subclasses__

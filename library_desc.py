11c224d6eebed9c3f763607084bce797e7e242ab
Gavin Stark <gjstark@me.com>
2020-05-23 20:27:46 +0100

    [library_desc] changed include_dirs and added verilog files

--- library_desc.py
+++ library_desc.py
@@ -1,5 +1,5 @@
 import cdl_desc
-from cdl_desc import CdlModule, CModel
+from cdl_desc import CdlModule, CModel, Verilog
 
 class Library(cdl_desc.Library):
     name="std"
@@ -10,8 +10,8 @@ class TechModules(cdl_desc.Modules):
     c_src_dir   = "cmodel"
     src_dir     = "cdl"
     tb_src_dir  = "tb_cdl"
-    include_dir = "cdl"
-    export_dirs = [ src_dir, include_dir ]
+    cdl_include_dirs = ["cdl"]
+    export_dirs      = cdl_include_dirs + [ src_dir ]
     modules = []
     modules += [ CdlModule("tech_sync_bit") ]
     modules += [ CdlModule("tech_sync_flop") ]
@@ -22,8 +22,16 @@ class SramModules(cdl_desc.Modules):
     c_src_dir   = "cmodel"
     src_dir     = "cdl"
     tb_src_dir  = "tb_cdl"
-    include_dir = "cdl"
-    export_dirs = [ src_dir, include_dir ]
+    cdl_include_dirs = ["cdl"]
+    export_dirs      = cdl_include_dirs + [ src_dir ]
     modules = []
     modules += [ CModel("srams",src_dir=c_src_dir) ]
+    pass
+
+class VerilogModules(cdl_desc.Modules):
+    name = "sram_verilog"
+    src_dir     = "verilog"
+    modules = []
+    modules += [ Verilog("srw_srams") ]
+    modules += [ Verilog("mrw_srams") ]
     pass

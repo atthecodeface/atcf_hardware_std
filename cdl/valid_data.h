198601745d9486420446ab1c97a3030eca75e0ec
Gavin Stark <atthecodeface@gmail.com>
2025-03-05 10:38:27 +0000

    Added valid_data header file

---
+++ cdl/valid_data.h
@@ -0,0 +1,24 @@
+/*t t_vdata_8 */
+typedef struct {
+    bit valid;
+    bit[8] data;
+} t_vdata_8;
+
+/*t t_vdata_16 */
+typedef struct {
+    bit valid;
+    bit[16] data;
+} t_vdata_16;
+
+/*t t_vdata_32 */
+typedef struct {
+    bit valid;
+    bit[32] data;
+} t_vdata_32;
+
+/*t t_vdata_64 */
+typedef struct {
+    bit valid;
+    bit[64] data;
+} t_vdata_64;
+
